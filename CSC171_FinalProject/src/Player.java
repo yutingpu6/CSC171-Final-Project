62a7ee92286156c316796d1c4e91a4b83dadae97
Yuting Pu <ytingp56@gmail.com>
2024-03-28 13:22:35 -0400

    Added more methods taken from Workshop 7

--- CSC171_FinalProject/src/Player.java
+++ CSC171_FinalProject/src/Player.java
@@ -6,14 +6,15 @@ import javax.swing.JComponent;
 public class Player extends JComponent {
 	private int hp;
 	private boolean isAlive;
-	private double x;
-	private double y;
+	private double xPos, yPos, velocityX, velocityY;
 	
-	public Player(int hp, boolean isAlive, double x, double y) {
+	public Player(int hp, boolean isAlive, double xPos, double yPos, double velocityX, double velocityY) {
 		this.hp = hp;
 		this.isAlive = isAlive;
-		this.x = x;
-		this.y = y;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.velocityX = velocityX;
+		this.velocityY = velocityY;
 	}
 	
 	public int getHp() {
@@ -24,6 +25,15 @@ public class Player extends JComponent {
 		return isAlive;
 	}
 	
+	public double getXPos() {
+		return xPos; // Don't wanna interfere with JComponent existing
+					// method getX(), so I named the variables xPos and yPos
+	}
+	
+	public double getYPos() {
+		return yPos;
+	}
+	
 	public void setHp(int dmg) {
 		hp -= dmg;
 	}
@@ -31,5 +41,39 @@ public class Player extends JComponent {
 	public void setAlive() {
 		if (hp==0)
 			isAlive = false;
+	}
+	
+	// Most of this is taken from Workshop 7 code
+	
+	public void updatePosition(double time) {
+		xPos += velocityX * time;
+		yPos += velocityY * time;
+	}
+	
+	public void setVelocity(double vX, double vY) {
+		velocityX = vX;
+		velocityY = vY;
+	}
+	
+	public double distance(double x, double y, double x2, double y2) {
+		double xSum = (x-x2)*(x-x2);
+		double ySum = (y-y2)*(y-y2);
+		
+		return Math.sqrt(xSum + ySum);
+	}
+	
+	public boolean detectCollision(Player other) {
+		if(distance(xPos, yPos, other.getXPos(), other.getYPos()) <= 0)
+			return true;
+		else
+			return false;
+	}
+	
+	// Do what on collision? Probably not bounce, maybe just stop in place?
+	// Is that like setting velocity to 0?
+	
+	public void resolveCollision(Player other) {
+		if (detectCollision(other) == true)
+			setVelocity(0,0);
 	}
 }
